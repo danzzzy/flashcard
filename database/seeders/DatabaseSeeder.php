31f37d9e692a5f85d0865a185b98a791a0ebf000
Daniel Rabottini <da_nkr@live.com>
2022-05-16 22:19:16 +0900

    push master

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use database/seeders/CardSeeder.php;
 
 class DatabaseSeeder extends Seeder
 {
